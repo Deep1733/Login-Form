e823374a5d6f593a576648c399ddaa7113916840
Deep Maurya <deepmaurya1733@gmail.com>
2023-03-08 13:19:27 +0530

    final

--- validation.js
+++ validation.js
@@ -1,4 +1,5 @@
-function myfunction(){
+function showpass()
+{
     var x =document.getElementById("pass");
     if(x.type === "password"){
         x.type = "text";
@@ -11,13 +12,19 @@ function myfunction(){
 function validate(){
     var password = document.getElementById("pass");
     var length = document.getElementById("length");
-
-    if(password.value.length >= 6){
+    if(password.value.length >= 6  )
+    {
         alert("Login Succesfull");
         window.location.replace("newpage.html");
         return false;
     }
-    else{
+    else
+    {
         alert("Please check the credentials and try again");
     }
+}
+
+function logout()
+{
+    window.location.replace("index.html");
 }
